178d98b86df7f78fd3b3c06712984c2cfc3be143
ANYACHOKHANY365CODE <69138478+ANYACHOKHANY365CODE@users.noreply.github.com>
2025-06-30 23:11:43 +0530

    Update write-key.js

--- write-key.js
+++ write-key.js
@@ -1,5 +1,7 @@
-// write-key.js
 const fs = require('fs');
+if (!process.env.SERVICE_ACCOUNT_KEY) {
+  throw new Error('SERVICE_ACCOUNT_KEY environment variable is not set!');
+}
 fs.writeFileSync(
   'serviceAccountKey.json',
   process.env.SERVICE_ACCOUNT_KEY.replace(/\\n/g, '\n')
